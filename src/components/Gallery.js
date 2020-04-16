d8212f7054ec4e2164c943152ddbb99524e0b556
Matt Braly <mattbraly2015@gmail.com>
2020-04-16 10:10:39 -0400

    second push for greg

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,28 +5,27 @@ import GalleryCard from './GalleryCard';
 export default function Gallery(props) {
     const [loaded, setLoaded] = useState(true);
     const [galList, setGalList] = useState([]);
-    let galLister = undefined
     const makeGallery = async () => {
 
         const response = await fetch("http://127.0.0.1:5000/makeGallery");
         console.log(response)
         const data = await response.json();
         console.log(data)
-        galLister = data.names.map((name) => {
-            return (
-                <GalleryCard name={name}/>
-            )})
-        setGalList(galLister)
+        setGalList(data)
+        
+        
     }
-
+    
     // useEffect(() => {
     //     makeGallery()
     // },[loaded])
-    if (galList.length < 1) {
-        makeGallery()
-      }
+    // if (galList.length < 1) {
+    //     makeGallery()
+    //     time.sleep(5)
+    //   }
     return (
         <div>
+            <button onClick={makeGallery}>Click Button</button>
         <h2>Gallery of Logos</h2>
         <div>{galList}</div>
         </div>
